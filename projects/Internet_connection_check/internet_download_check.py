089ee77be3f5fc85264e739a2474595afb930203
charislzq <charis.lim.2020@scis.smu.edu.sg>
2023-12-13 12:51:36 +0800

    added speedtest module

--- projects/Internet_connection_check/internet_download_check.py
+++ projects/Internet_connection_check/internet_download_check.py
@@ -1,50 +1,86 @@
-import requests
-from time import time
-from requests.exceptions import ConnectionError
+# Method 1: using speedtest module
+import speedtest
 
-def download_speed_test(url='https://datacatalogfiles.worldbank.org/ddh-published/0061111/DR0085897/02--CHAPTER%202.rar?versionId=2023-01-18T19:02:19.7709485Z'):
-  """
-  Tests the download speed of the given URL.
+#creates an instance of the Speedtest class
+#interact with the speedtest module and perform measurements
+st = speedtest.Speedtest()
+#retrieves information about available speedtest servers 
+#chooses the one that is considered to be the "best" for the current location based on factors like ping and latency
+best_server = st.get_best_server()
 
-  Args:
-    url: The URL to download the data from.
+print(f"Testing download speed from server {best_server['host']}...")
+#initiates a download test from the chosen server and measures the download speed
+#bytes per second
+download_speed = st.download()
 
-  Returns:
-    A tuple containing the download speed in megabits per second (Mbps) and the response status code.
-  """
-  start_time = time()
+#divides download speed in bytes per second by 1024**2 to convert it to megabits per second (Mbps)
+# 1 byte = 8 bits - not sure if need to divide by 8 as well
+# 1 kilobyte = 1024 bytes
+# 1 megabit = 1024 bits
+print(f"Download Speed: {download_speed / (1024**2):.2f} Mbps")
 
-  try:
-    response = requests.get(url, stream=True, timeout=5)
-    content_length = int(response.headers.get('content-length', 0))
 
-    if content_length > 0:
-      downloaded_bytes = 0
-      for chunk in response.iter_content(chunk_size=1024):
-        downloaded_bytes += len(chunk)
-      download_time = time() - start_time
+# ----------------------------------------------------------------------------------------
+# Method 2: manual calculation
+# import requests
+# from time import time
+# from requests.exceptions import ConnectionError
 
-      # Convert bytes to megabits and calculate speed
-      download_speed = (downloaded_bytes * 8) / (download_time * 1024 * 1024)
-      return download_speed, response.status_code
-    else:
-      print(f'Failed to determine content length for {url}.')
-      return 0, response.status_code
-  except ConnectionError as e:
-    print(f'Failed to connect to {url}.')
-    return 0, None
-  except Exception as e:
-    print(f'Error while testing download speed: {e}')
-    return 0, None
+# def download_speed_test(url='https://datacatalogfiles.worldbank.org/ddh-published/0061111/DR0085897/02--CHAPTER%202.rar?versionId=2023-01-18T19:02:19.7709485Z'):
+#   """
+#   Tests the download speed of the given URL.
 
-# Get download speed and status code
-download_speed, status_code = download_speed_test()
+#   Args:
+#     url: The URL to download the data from.
 
-if download_speed > 0:
-  print(f'Download speed: {download_speed:.2f} Mbps')
-else:
-  print(f'Failed to measure download speed.')
+#   Returns:
+#     A tuple containing the download speed in megabits per second (Mbps) and the response status code.
+#   """
+#   #save current time as reference point
+#   start_time = time() 
 
-if status_code:
-  print(f'Status code: {status_code}')
+#   try:
+#     #sends a GET req to the URL, download data in chunks, time out 5s
+#     response = requests.get(url, stream=True, timeout=5) 
+#     #get content length from service resp header, if none, set to 0
+#     content_length = int(response.headers.get('content-length', 0))
+
+#     if content_length > 0:
+#       #keep track of bytes downloaded
+#       downloaded_bytes = 0
+#       # iterate over downloaded data in chunks of 1024 bytes
+#       # 1024 bytes: compatability (Many network protocols and libraries are designed to work with data chunks of specific sizes.)
+#       for chunk in response.iter_content(chunk_size=1024):
+#         #update downloaded_bytes counter
+#         downloaded_bytes += len(chunk)
+#       #calculate download time from current time - start time
+#       download_time = time() - start_time
+#       print(download_time)
+
+#       # Convert bytes to megabits (Mb) and calculate speed in Mbps
+#       # downloaded_bytes * 8: bytes to bits (1 byte = 8 bits)
+#       # download_time * 1024 * 1024: 
+#       download_speed = (downloaded_bytes * 8) / (download_time * 1024 * 1024)
+#       return download_speed, response.status_code
+#     else:
+#       print(f'Failed to determine content length for {url}.')
+#       return 0, response.status_code
+#   except ConnectionError as e:
+#     print(f'Failed to connect to {url}.')
+#     return 0, None
+#   except Exception as e:
+#     print(f'Error while testing download speed: {e}')
+#     return 0, None
+
+# # Get download speed and status code
+# download_speed, status_code = download_speed_test()
+
+# if download_speed > 0:
+#   print(f'Download speed: {download_speed:.2f} Mbps')
+# else:
+#   print(f'Failed to measure download speed.')
+
+# if status_code:
+#   print(f'Status code: {status_code}')
+
 
